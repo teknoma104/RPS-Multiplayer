689330421b4d927d4b0aec1bc99a88fee5c0ca4e
Tommy Le <teknoma104@gmail.com>
2018-07-17 19:15:53 -0500

    Initial Files.

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB0RyNjY9rlD93xyk1VK3bIYisAQOZPr6I",
+    authDomain: "myrpsproject.firebaseapp.com",
+    databaseURL: "https://myrpsproject.firebaseio.com",
+    projectId: "myrpsproject",
+    storageBucket: "myrpsproject.appspot.com",
+    messagingSenderId: "811242828966"
+  };
+  
+  firebase.initializeApp(config);
